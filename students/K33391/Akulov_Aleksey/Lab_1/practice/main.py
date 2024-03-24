882f1ba0af98b58a91a4ed9fda01c89cfb528720
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2024-03-24 21:45:39 +0300

    practice 2

--- students/K33391/Akulov_Aleksey/Lab_1/practice/main.py
+++ students/K33391/Akulov_Aleksey/Lab_1/practice/main.py
@@ -1,10 +1,14 @@
-from typing import List, Optional
+from http.client import HTTPException
 
+from typing import List, Optional
 from typing_extensions import TypedDict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
 
-from models import Warrior, Profession
+from models import Warrior, Profession, WarriorDefault, ProfessionDefault, \
+    Skill, SkillDefault
+from connections import init_db, get_session
 
 app = FastAPI()
 
@@ -53,20 +57,28 @@ professions_db = [
 ]
 
 
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+
 @app.get("/warriors_list")
-def warriors_list() -> List[Warrior]:
-    return temp_bd
+def warriors_list(session=Depends(get_session)) -> List[Warrior]:
+    return session.exec(select(Warrior)).all()
 
 
 @app.get("/warrior/{warrior_id}")
-def warriors_get(warrior_id: int) -> List[Warrior]:
-    return [warrior for warrior in temp_bd if warrior.get("id") == warrior_id]
+def warriors_get(warrior_id: int, session=Depends(get_session)) -> Warrior:
+    return session.exec(select(Warrior).where(Warrior.id == warrior_id)).first()
 
 
 @app.post("/warrior")
-def warriors_create(warrior: Warrior) -> TypedDict('Response', {"status": int, "data": Warrior}):
-    warrior_to_append = warrior.model_dump()
-    temp_bd.append(warrior_to_append)
+def warriors_create(warrior: WarriorDefault, session=Depends(get_session)) -> TypedDict('Response', {"status": int,
+                                                                                                     "data": Warrior}):
+    warrior = Warrior.model_validate(warrior)
+    session.add(warrior)
+    session.commit()
+    session.refresh(warrior)
     return {"status": 200, "data": warrior}
 
 
@@ -79,47 +91,89 @@ def warrior_delete(warrior_id: int):
     return {"status": 201, "message": "deleted"}
 
 
-@app.put("/warrior{warrior_id}")
-def warrior_update(warrior_id: int, warrior: Warrior) -> List[Warrior]:
-    for war in temp_bd:
-        if war.get("id") == warrior_id:
-            warrior_to_append = warrior.model_dump()
-            temp_bd.remove(war)
-            temp_bd.append(warrior_to_append)
-    return temp_bd
+@app.patch("/warrior{warrior_id}")
+def warrior_update(warrior_id: int, warrior: WarriorDefault, session=Depends(get_session)) -> WarriorDefault:
+    db_warrior = session.get(Warrior, warrior_id)
+    if not db_warrior:
+        raise HTTPException(status_code=404, detail="Warrior not found")
+    warrior_data = warrior.model_dump(exclude_unset=True)
+    for key, value in warrior_data.items():
+        setattr(db_warrior, key, value)
+    session.add(db_warrior)
+    session.commit()
+    session.refresh(db_warrior)
+    return db_warrior
 
 
-@app.get("/professions/", response_model=List[Profession])
-def get_professions():
-    return professions_db
+@app.delete("/warrior/delete{warrior_id}")
+def warrior_delete(warrior_id: int, session=Depends(get_session)):
+    warrior = session.get(Warrior, warrior_id)
+    if not warrior:
+        raise HTTPException(status_code=404, detail="Warrior not found")
+    session.delete(warrior)
+    session.commit()
+    return {"ok": True}
 
-@app.get("/professions/{profession_id}", response_model=Optional[Profession])
-def get_profession(profession_id: int):
-    for profession in professions_db:
-        if profession["id"] == profession_id:
-            return profession
-    return None
+@app.get("/professions_list")
+def professions_list(session=Depends(get_session)) -> List[Profession]:
+    return session.exec(select(Profession)).all()
 
-@app.post("/professions/", response_model=Profession, status_code=201)
-def create_profession(profession: Profession):
-    profession_dict = profession.dict()
-    profession_dict["id"] = len(professions_db) + 1
-    professions_db.append(profession_dict)
-    return profession_dict
 
-@app.put("/professions/{profession_id}", response_model=Optional[Profession])
-def update_profession(profession_id: int, profession: Profession):
-    for i, prof in enumerate(professions_db):
-        if prof["id"] == profession_id:
-            update_profession_dict = profession.dict()
-            professions_db[i] = update_profession_dict
-            return update_profession_dict
-    return None
+@app.get("/profession/{profession_id}")
+def profession_get(profession_id: int, session=Depends(get_session)) -> Profession:
+    return session.get(Profession, profession_id)
 
-@app.delete("/professions/{profession_id}", status_code=204)
-def delete_profession(profession_id: int):
-    for i, prof in enumerate(professions_db):
-        if prof["id"] == profession_id:
-            del professions_db[i]
-            return {"message": "Profession deleted"}
-    return {"message": "Profession not found"}
+
+@app.post("/profession")
+def profession_create(prof: ProfessionDefault, session=Depends(get_session)) -> TypedDict('Response', {"status": int,
+                                                                                                     "data": Profession}):
+    prof = Profession.model_validate(prof)
+    session.add(prof)
+    session.commit()
+    session.refresh(prof)
+    return {"status": 200, "data": prof}
+
+
+@app.post("/skills/")
+def create_skill(
+    skill: SkillDefault, session: Session = Depends(get_session)
+):
+    skill = Skill.model_validate(skill)
+    session.add(skill)
+    session.commit()
+    session.refresh(skill)
+    return {"status": 200, "data": skill}
+
+
+@app.get("/skills/{skill_id}")
+def read_skill(skill_id: int, session: Session = Depends(get_session)):
+    skill = session.get(Skill, skill_id)
+    if skill is None:
+        raise HTTPException(status_code=404, detail="Skill not found")
+    return skill
+
+
+@app.patch("/skills/{skill_id}")
+def update_skill(
+    skill_id: int, skill: SkillDefault, session: Session = Depends(init_db)
+):
+    db_skill = session.get(Skill, skill_id)
+    if db_skill is None:
+        raise HTTPException(status_code=404, detail="Skill not found")
+    skill_data = skill.model_dump(exclude_unset=True)
+    for key, value in skill_data.items():
+        setattr(db_skill, key, value)
+    session.add(db_skill)
+    session.commit()
+    session.refresh(db_skill)
+    return db_skill
+
+
+@app.delete("/skills/{skill_id}")
+def skill_delete(skill_id: int, session: Session = Depends(get_session)):
+    skill = session.get(Skill, skill_id)
+    if not skill:
+        raise HTTPException(status_code=404, detail="Skill not found")
+    session.delete(skill)
+    session.commit()
+    return {"ok": True}
--- students/K33391/Akulov_Aleksey/Lab_1/practice/models.py
+++ students/K33391/Akulov_Aleksey/Lab_1/practice/models.py
@@ -1,7 +1,27 @@
 from enum import Enum
 from typing import Optional, List
 
-from pydantic import BaseModel
+#from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class SkillDefault(SQLModel):
+    name: str
+    description: Optional[str] = ""
+
+class SkillWarriorLink(SQLModel, table=True):
+    skill_id: Optional[int] = Field(
+        default=None, foreign_key="skill.id", primary_key=True
+    )
+    warrior_id: Optional[int] = Field(
+        default=None, foreign_key="warrior.id", primary_key=True
+    )
+
+class Skill(SkillDefault, table=True):
+    id: int = Field(default=None, primary_key=True)
+    warriors: Optional[List["Warrior"]] = Relationship(
+        back_populates="skills", link_model=SkillWarriorLink
+    )
 
 class RaceType(Enum):
     director = "director"
@@ -9,22 +29,25 @@ class RaceType(Enum):
     junior = "junior"
 
 
-class Profession(BaseModel):
-    id: int
+class ProfessionDefault(SQLModel):
     title: str
     description: str
 
 
-class Skill(BaseModel):
-    id: int
-    name: str
-    description: str
+class Profession(ProfessionDefault, table=True):
+    id: int = Field(default=None, primary_key=True)
+    warriors_prof: List["Warrior"] = Relationship(back_populates="profession")
 
-class Warrior(BaseModel):
-    id: int
+
+class WarriorDefault(SQLModel):
     race: RaceType
     name: str
     level: int
-    profession: Profession
-    skills: Optional[List[Skill]] = []
+    profession_id: Optional[int] = Field(default=None, foreign_key="profession.id")
+
+
+class Warrior(WarriorDefault, table=True):
+    id: int = Field(default=None, primary_key=True)
+    profession: Optional[Profession] = Relationship(back_populates="warriors_prof")
+    skills: Optional[List[Skill]] = Relationship(back_populates="warriors", link_model=SkillWarriorLink)
 
