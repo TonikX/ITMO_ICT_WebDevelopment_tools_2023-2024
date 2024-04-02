b51767e568f0eda33882ec8038a2ce26d60c854c
7Alexandra8 <alexp20030708@gmail.com>
2024-04-02 03:34:42 +0300

    Practice(забыла выложить на гит)

---
+++ students/K33392/Pronina_Alexandra/Practices/Task_2/connection.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Session, create_engine
+from cruds import USERNAME, PASSWORD
+
+db_url = f'postgresql://{USERNAME}:{PASSWORD}@localhost:5432/Task_1'
+engine = create_engine(db_url, echo=True)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- students/K33392/Pronina_Alexandra/Practices/Task_3/migrations/env.py
+++ students/K33392/Pronina_Alexandra/Practices/Task_3/migrations/env.py
@@ -1,8 +1,7 @@
 from logging.config import fileConfig
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from sqlmodel import SQLModel
-from models import *
 
 from alembic import context
 
@@ -19,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLModel.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
