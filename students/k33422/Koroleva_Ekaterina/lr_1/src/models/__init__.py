940fe00fdf92f86255941535f37fc95c8c7cb5b0
KorolevaEkaterina334740 <trina696queen@gmail.com>
2024-04-28 12:56:17 +0300

    create init db tables

--- students/k33422/Koroleva_Ekaterina/lr_1/src/models/__init__.py
+++ students/k33422/Koroleva_Ekaterina/lr_1/src/models/__init__.py
@@ -1,1 +1,5 @@
 from .base import *
+from .user import *
+from .profile import *
+from .trip import *
+from .participant import *
---
+++ students/k33422/Koroleva_Ekaterina/lr_1/src/models/profile.py
@@ -0,0 +1,41 @@
+from datetime import date
+from enum import Enum
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text, ForeignKey, Enum as SQLEnum
+from .base import Base
+
+
+__all__ = ['Gender', 'Profile']
+
+if TYPE_CHECKING:
+    from .user import User
+    from .trip import Trip
+    from .participant import Participant
+
+
+class Gender(str, Enum):
+    MALE = 'male'
+    FEMALE = 'female'
+
+
+class Profile(Base):
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id', ondelete='CASCADE'), unique=True
+    )
+    first_name: Mapped[str] = mapped_column(String(32))
+    last_name: Mapped[str] = mapped_column(String(32))
+    middle_name: Mapped[str] = mapped_column(String(32))
+    birth_date: Mapped[date] = mapped_column()
+    gender: Mapped[Gender] = mapped_column(SQLEnum(Gender))
+    bio: Mapped[str] = mapped_column(Text, default='', server_default='')
+    country: Mapped[str] = mapped_column(String(64))
+    city: Mapped[str | None] = mapped_column(String(64), nullable=True)
+    language: Mapped[str] = mapped_column(String(16))
+
+    user: Mapped['User'] = relationship(back_populates='profile')
+    trips: Mapped[list['Trip']] = relationship(
+        secondary='participants', back_populates='profiles'
+    )
+    trips_details: Mapped[list['Participant']] = relationship(back_populates='profile')
