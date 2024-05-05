64c3db0b943096e2ad57fd85ed955dd30cb9dc12
Lena Skorokhodova <87618005+HelBroone@users.noreply.github.com>
2024-05-06 00:23:44 +0300

    edit schemas

--- students/k33392/Skorokhodova_Elena/Lr1/travel_app/schemas.py
+++ students/k33392/Skorokhodova_Elena/Lr1/travel_app/schemas.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class UserProfile(Base):
     __tablename__ = "user_profiles"
@@ -53,3 +58,73 @@ class TripReview(Base):
 
     user = relationship("User")
     trip = relationship("Trip", back_populates="reviews")
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserProfileBase(BaseModel):
+    username: Optional[str] = None
+    skills: Optional[str] = None
+    experience: Optional[str] = None
+    preferences: Optional[str] = None
+
+
+class UserProfileCreate(UserProfileBase):
+    pass
+
+
+class UserProfileUpdate(UserProfileBase):
+    pass
+
+
+class TripBase(BaseModel):
+    owner_id: int
+    departure_location: str
+    destination: str
+    start_date: datetime
+    end_date: datetime
+    duration: int
+    details: Optional[str] = None
+
+
+class TripCreate(TripBase):
+    pass
+
+
+class TripUpdate(TripBase):
+    pass
+
+
+class UserTripBase(BaseModel):
+    user_id: int
+    trip_id: int
+    role: Optional[str] = None
+
+
+class UserTripCreate(UserTripBase):
+    pass
+
+
+class TripReviewBase(BaseModel):
+    trip_id: int
+    user_id: int
+    rating: int
+    comment: Optional[str] = None
+
+
+class TripReviewCreate(TripReviewBase):
+    pass
