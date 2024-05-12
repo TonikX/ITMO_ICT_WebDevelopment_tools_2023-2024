a1ff6a84d5213a9477c2d7121b9e144d18e127a7
Mesh296 <minhhan181@gmail.com>
2024-05-13 00:59:21 +0300

    fix: use aiohttp in asyncio

--- student/K33392/Чан_Дык_Минь/laboratory/lab_2/task_2/asyncio_parse.py
+++ student/K33392/Чан_Дык_Минь/laboratory/lab_2/task_2/asyncio_parse.py
@@ -3,14 +3,18 @@ import time
 from db import init_db, create_trip, TripDefault
 from typing import List
 from datetime import datetime, date
-from bs4 import BeautifulSoup # type: ignore
-import requests  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+import aiohttp # type: ignore
 from urls import URLS
 
+async def fetch_content(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
 
 async def parse_trip(url: str) -> TripDefault:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    content = await fetch_content(url)
+    soup = BeautifulSoup(content, 'html.parser')
     title = soup.title.string
     date_start = date.today()
     date_end = date.today()
@@ -23,6 +27,7 @@ async def parse_trip(url: str) -> TripDefault:
 
     trip_status = "Asyncio"
     other_details = "The link to the trip is: " + url
+    
     return TripDefault(
         title=title, 
         date_start=date_start, 
@@ -45,10 +50,12 @@ async def main():
         task = asyncio.create_task(save_trips(url))
         tasks.append(task)
     
+    # Звездочка * перед tasks —  распаковывает список
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     init_db()
     start_time = time.time()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
     print(f"Execution time using Asyncio: {time.time() - start_time}")
