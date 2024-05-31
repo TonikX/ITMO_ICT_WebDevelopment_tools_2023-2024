50c6c7d0d2e802634933453a63d982749ef24e86
jarvis394 <tarnatovski@gmail.com>
2024-05-31 19:25:24 +0300

    Add solution to lab2_2

--- students/K33402/Ekushev_Vlad/labs/lab2/migrations/env.py
+++ students/K33402/Ekushev_Vlad/labs/lab2/migrations/env.py
@@ -32,7 +32,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.main import Balance, Category, Target, SQLModel, User, Transactions
+target_metadata = SQLModel.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
